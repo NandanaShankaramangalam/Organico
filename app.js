65ed35a4ac286eb7192a0443e74f5dccdf3c0a0a
Nandana <nandanasbabu1999@gmail.com>
2023-04-04 14:19:31 +0530

    updates

--- app.js
+++ app.js
@@ -5,28 +5,23 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 const fileUpload = require('express-fileupload');
-const hbs=require('express-handlebars')
-// 
-// const mongoose = require('mongoose');
-// 
+const hbs = require('express-handlebars')
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 
 var app = express();
 
 // view engine setup
-app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials'}))
+app.engine('hbs', hbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout/', partialsDir: __dirname + '/views/partials' }))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(session({secret:"key",saveUninitialized:true,resave:false,cookie:{maxAge:60000*60}}));
+app.use(session({ secret: "key", saveUninitialized: true, resave: false, cookie: { maxAge: 60000 * 60 } }));
 app.use(fileUpload({
-  useTempFiles : true,
-  tempFileDir : __dirname+'/public/temp'
+  useTempFiles: true,
+  tempFileDir: __dirname + '/public/temp'
 }));
 
-
-
-app.use(logger('dev'));  
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -37,31 +32,31 @@ app.use('/admin', adminRouter);
 var db = require("./config/connection");
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 //defined functions
 let hb = hbs.create({})
-hb.handlebars.registerHelper('eq',function(a,b){
+hb.handlebars.registerHelper('eq', function (a, b) {
   return a == b;
 });
 
 
-hb.handlebars.registerHelper('gte',function(a,b){
+hb.handlebars.registerHelper('gte', function (a, b) {
   return a >= b;
 });
 
-hb.handlebars.registerHelper('mul',function(a,b){
-  return a*b;
+hb.handlebars.registerHelper('mul', function (a, b) {
+  return a * b;
 });
 
-hb.handlebars.registerHelper('inc',function(a,b){
+hb.handlebars.registerHelper('inc', function (a, b) {
   return parseInt(a) + 1;
 });
 
-// error handler
-app.use(function(err, req, res, next) {
+  // error handler
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -70,11 +65,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   console.log(err);
   let route = (req.url).split("/")[1]
-    if(route=="admin")
-        admin = true
-    else
-        admin = false
-  res.render('error',{admin});
+  if (route == "admin")
+    admin = true
+  else
+    admin = false
+  res.render('error', { admin });
 });
 
 module.exports = app;
